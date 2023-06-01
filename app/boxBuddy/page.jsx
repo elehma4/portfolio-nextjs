0a0489656c92cc6135d1dce1d69e0eaa53675ce5
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-01 08:40:50 -0500

    boxBuddy details

--- app/boxBuddy/page.jsx
+++ app/boxBuddy/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/Navbar';
 import Image from 'next/image';
-import boxBuddyImg from '../../public/assets/projects/boxBuddy-main.png'
+import boxBuddyImg2 from '../../public/assets/projects/boxBuddy-profile1.png'
 import {RiRadioButtonFill} from 'react-icons/ri'
 import Link from 'next/link'
 
@@ -13,10 +13,10 @@ function boxBuddy() {
       <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
         <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
 
-        <Image className='absolute z-1' layout='fill' objectFit='cover' src={boxBuddyImg} alt='/' />
+        <Image className='absolute z-1' layout='fill' objectFit='cover' src={boxBuddyImg2} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
             <h2 className='py-2'>Box Buddy</h2>
-            <h3>Javascript / Express.js / HTML / CSS</h3>
+            <h3>Javascript / Tailwind CSS / PostgreSQL</h3>
         </div>
       </div>
 
@@ -33,9 +33,18 @@ function boxBuddy() {
             <p className='text-center font-bold pb-2'>Technologies</p>
             <div className='grid grid-cols-3 md:grid-cols-1'>
               <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> JavaScript </p>
-              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> HTML </p>
-              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> CSS </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Tailwind CSS </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Node.js </p>
               <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Express.js </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Sequelize.js </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> PostgreSQL </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Passport </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> bcryptjs </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Cookie Session </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Database GUI: Beekeeper </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Cloudinary </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Multer </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> API: API-Ninjas </p>
             </div>
           </div>
         </div>
