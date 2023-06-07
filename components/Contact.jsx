b615eb555cccc6c9b71df3ac3363a3806d3f62a3
Ethan <117924723+ETH1Elohim@users.noreply.github.com>
2023-06-07 16:15:36 -0500

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -56,7 +56,7 @@ function Contact() {
             <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
                 <div className='p-4'>
 
-                    <form action="mailto:elehman.development@gmail.com" method="post" enctype="text/plain">
+                    <form action='https://formspree.io/f/xrgvwrly' method='post'>
                         <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                             <div className='flex flex-col'>
                                 <label className='uppercase text-sm py-2'>Name</label>
