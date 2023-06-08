19a622d08e04068d16eee79fcc1427cc4061e221
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-07 20:48:05 -0500

    update about

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
                 <h2 className='py-4'>Who I am</h2>
-                <p className='py-2 text-gray-600'>Full-stack software engineer with a passion for decentralized finance and the transformational potential of blockchain technology. I am looking to utilize my strengths in web development and understanding of blockchain technologies to contribute to a protocol or company that is looking make a positive impact in Crypto.</p>
+                <p className='py-2 text-gray-600'>Full-stack software engineer with a passion for decentralized finance and the transformational potential of blockchain technology. I am looking to utilize my strengths in web development and understanding of blockchain technologies to contribute to a protocol or company that is looking to make a positive impact in Crypto.</p>
                 <Link href='/#projects'><p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p></Link>
             </div>
             
