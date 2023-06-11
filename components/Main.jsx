739b60feed11ef2162161f5785b59baacb023eae
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-11 17:48:22 -0500

    updated about me

--- components/Main.jsx
+++ components/Main.jsx
@@ -14,7 +14,7 @@ function Main() {
                 <p className='uppercase text-sm tracking-widest text-gray-600'>LET'S BUILD SOMETHING TOGETHER</p>
                 <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#5651e5]'>Ethan</span></h1>
                 <h1 className='py-4 text-gray-700'>A Full-Stack Software Engineer.</h1>
-                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>Dedicated full-stack software engineer proficient in front-end design, server-side logic, and back-end routing/debugging.</p>
+                <p className='py-4 text-gray-600 max-w-[70%] m-auto'>Versatile full-stack developer with a focus on delivering meaningful and impactful user experiences, proficient in both front-end design and back-end development.</p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300'>
                         <Link href='https://www.linkedin.com/in/ethan-lehman-web3/'><FaLinkedinIn /></Link>
