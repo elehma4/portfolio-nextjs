895664a654054f995e15dc26a16ee9c8a39c3b97
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-11 17:54:33 -0500

    update about me

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
                 <h2 className='py-4'>Who I am</h2>
-                <p className='py-2 text-gray-600'>Versatile full-stack developer with a focus on delivering meaningful and impactful user experiences, proficient in both front-end design and back-end development. I have a passion for leveraging technology to create positive impacts and prioritize security for web users. I am seeking opportunities to utilize my expertise in web development to contribute to innovative projects that enhance user experience, privacy, real-world interoperability, and digital freedom.</p>
+                <p className='py-2 text-gray-600'>I am a full-stack software engineer with a passion for leveraging technology to create positive impacts and prioritize security for web users. I am seeking opportunities to utilize my strengths and expertise in web development to contribute to innovative projects that enhance user experience, privacy, real-world interoperability, and digital freedom.</p>
                 <Link href='/#projects'><p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</p></Link>
             </div>
             
