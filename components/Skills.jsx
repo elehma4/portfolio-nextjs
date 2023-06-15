a8c9bec07d85b3d2a4206125787a672bf3aa3461
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-15 10:54:51 -0500

    add redux

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -156,10 +156,10 @@ function Skills() {
              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                     <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                         <div className='m-auto'>
-                            <Image src='/assets/skills/git.png' width={64} height={64} alt='/' />
+                            <Image src='/assets/skills/redux.png' width={64} height={64} alt='/' />
                         </div>
                         <div className='flex flex-col items-center justify-center'>
-                            <h3>Git</h3>
+                            <h3>Redux</h3>
                         </div>
 
                     </div>
