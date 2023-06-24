b7ce5a09bf5959a39dcf704e8ac206d423c8de85
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-23 20:56:44 -0500

    PATH link

--- app/pathwayFinance/page.jsx
+++ app/pathwayFinance/page.jsx
@@ -31,7 +31,7 @@ The entry point of the application, the 'Main.jsx' component, is built using Rea
 A variety of additional components, including 'Charts.jsx', 'Indices.jsx', 'Stocks.jsx', and 'Crypto.jsx', are responsible for rendering market-specific assets and illustrating trading data. These components leverage React's hooks and state management capabilities to manipulate chart data and handle window resize events for a responsive design. The 'News.jsx' component fetches and displays latest financial market news using the Alphavantage API, while the 'NavBar' component provides smooth, scroll-based navigation, demonstrating React's versatility and efficiency in handling various user interfaces and interactions.
 <br /><br />
 State management is handled using Redux Toolkit's 'favSlice', providing an intuitive way of handling favorites state and showcasing the simplicity and efficiency of Redux for state management in large applications. Overall, Pathway Finance serves as a testament to the successful application of modern front-end technologies and libraries to create a complex, responsive, and data-driven application. </p>
-          <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
+          <Link href='https://pathway-finance.netlify.app/'><button className='px-8 py-2 mt-4 mr-8'>Demo</button></Link>
           <Link href='https://github.com/ETH1Elohim/pathway-finance-reactjs'><button className='px-8 py-2 mt-4'>Code</button></Link>
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4 h-fit'>
