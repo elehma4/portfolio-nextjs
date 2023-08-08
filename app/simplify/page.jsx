43953f364b92a7854645716981b62309bab3399b
ETH1Elohim <ezmax.defi@gmail.com>
2023-08-08 13:13:36 -0700

    update projects

---
+++ app/simplify/page.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Navbar from '../../components/Navbar';
+import Image from 'next/image';
+import pathwayPortal from '../../public/assets/projects/pathway-portal.png'
+import {RiRadioButtonFill} from 'react-icons/ri'
+import Link from 'next/link'
+
+function boxBuddy() {
+  return (
+    <div className='w-full'>
+        <Navbar/>
+
+      <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
+
+        <Image className='absolute z-1' layout='fill' objectFit='cover' src={pathwayPortal} alt='/' />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+            <h2 className='py-2'>Pathway Finance (PATH)</h2>
+            <h3>React / Redux / Tailwind CSS</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>I am currently working alongside the lead developer at DecOps developing a product called SimpFi.Ai where users can automate operational workflows using AI.<br /> 
+          </p>
+          <Link href='https://pathway-finance.netlify.app/'><button className='px-8 py-2 mt-4 mr-8'>Demo</button></Link>
+          <Link href='https://github.com/ETH1Elohim/pathway-finance-reactjs'><button className='px-8 py-2 mt-4'>Code</button></Link>
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4 h-fit'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> JavaScript </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> React </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Redux (RTK) </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Tailwind CSS </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> Lightweight Charts by TradingView </p>
+              <p className='text-gray-600 py-2 flex items-center'><RiRadioButtonFill className='pr-1' /> APIs: Alpha Vantage & Twelve Data </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'><p className='underline cursor-pointer'>Back</p></Link>
+      </div>
+
+    </div>
+  )
+}
+
+export default boxBuddy
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,6 +8,7 @@ import boxBuddyImg from '../public/assets/projects/boxBuddy-main.png'
 import blackjackImg from '../public/assets/projects/blackjack1.png'
 import odeszaImg from '../public/assets/projects/odesza-forum.png'
 import portfolioImg from '../public/assets/projects/portfolio-home.png'
+import simplifyImg from '../public/assets/projects/SimpFi-AI.png'
 
 
 export const Projects = () => {
@@ -18,10 +19,11 @@ export const Projects = () => {
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
             <h2 className='py-4'>What I've Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
-              <ProjectItem title="Pathway Finance (PATH)" description="Front-End Market Analysis Tool built with React" backgroundImg={pathwayImg} projectUrl='/pathwayFinance' />
+              <ProjectItem title="SimpFi.AI" description="SimpFi.AI is a product where users can automate operational workflows using AI" backgroundImg={simplifyImg} projectUrl='/simplify' />
               <ProjectItem title='Box Buddy' description='Full-Stack Mobile Fitness Application' backgroundImg={boxBuddyImg} projectUrl='/boxBuddy' className='w-64 h-32' />
+              <ProjectItem title="Pathway Finance (PATH)" description="Front-End Market Analysis Tool built with React" backgroundImg={pathwayImg} projectUrl='/pathwayFinance' />
               <ProjectItem title='Hermes App' description='Front-End Crypto Application' backgroundImg={hermesImg} projectUrl='/hermesApp' />
-              <ProjectItem title='My Portfolio Site' description='Front-End Next.js Portfolio Site' backgroundImg={portfolioImg} projectUrl='/myPortfolio' />
+              {/* <ProjectItem title='My Portfolio Site' description='Front-End Next.js Portfolio Site' backgroundImg={portfolioImg} projectUrl='/myPortfolio' /> */}
               {/* <ProjectItem title='Odesza Forum' description='Odesza forums with Socket.io' backgroundImg={odeszaImg} projectUrl='/odeszaForum' /> */}
               {/* <ProjectItem title='Blackjack Game' description='Logic-based Blackjack Game' backgroundImg={blackjackImg} projectUrl='/blackjack' /> */}
               
